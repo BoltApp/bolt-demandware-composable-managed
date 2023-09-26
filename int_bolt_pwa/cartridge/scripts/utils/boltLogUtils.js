8e98fb46ccc0b86cda7dd49b4880f82f544ad05c
justinBolt1987 <jxie@bolt.com>
2023-09-26 12:51:36 -0400

    Add necessary endpoints and related functions

--- int_bolt_pwa/cartridge/scripts/utils/boltLogUtils.js
+++ int_bolt_pwa/cartridge/scripts/utils/boltLogUtils.js
@@ -17,3 +17,46 @@ exports.getLogger = function (category) {
 
     return Logger.getLogger(LogPrefix);
 };
+
+/**
+ * Mask customer's sensitive data, interaction function
+ * @param {Object} data - request object
+ * @returns {Object} masked data
+ */
+exports.maskCustomerData = function (data) {
+    var maskedData = JSON.stringify(data);
+    maskedData = actionMaskData(JSON.parse(maskedData));
+    return JSON.stringify(maskedData);
+};
+
+/**
+ * Mask customer's sensitive data - action function
+ * @param {Object} obj - request object
+ * @returns {Object} masked data
+ */
+function actionMaskData(obj) {
+    // eslint-disable-next-line
+    for (var key in obj) {
+        switch (key) {
+            case 'first_name':
+            case 'last_name':
+            case 'expiration':
+            case 'billing_address':
+            case 'shipping_address':
+            case 'phones':
+            case 'emails':
+                obj[key] = '***';
+
+                if (typeof (obj[key]) === 'object') {
+                    actionMaskData(obj[key]);
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return obj;
+}
