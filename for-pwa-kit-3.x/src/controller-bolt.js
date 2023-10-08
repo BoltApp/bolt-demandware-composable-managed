c581a0b05c929c88f593047adb95377e2679b684
Ning W <phpdev2012@126.com>
2023-10-08 09:20:59 +0800

    Update Bolt component 3.x to npm package

---
+++ for-pwa-kit-3.x/src/controller-bolt.js
@@ -0,0 +1,23 @@
+import {createControllerFetch} from './utils'
+import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
+
+class ControllerBolt {
+    constructor() {
+        const {app: appConfig} = getConfig()
+        this.fetch = createControllerFetch(appConfig.commerceAPI)
+    }
+
+    async getCartSession() {
+        return await this.fetch('Bolt-GetCartSession', 'GET')
+    }
+
+    async getBoltConfig() {
+        return await this.fetch('Bolt-GetBoltConfig', 'GET', 'force-cache')
+    }
+
+    async getDefaultShipmethod() {
+        return await this.fetch('Bolt-GetDefaultShip', 'GET')
+    }
+}
+
+export default ControllerBolt
